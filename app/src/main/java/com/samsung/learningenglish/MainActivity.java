136e8452eab2b976384412ac799c93eaa46102f9
Koryun <79407889+Koryun986@users.noreply.github.com>
2022-12-01 13:55:54 +0400

    Commit

--- app/src/main/java/com/samsung/learningenglish/MainActivity.java
+++ app/src/main/java/com/samsung/learningenglish/MainActivity.java
@@ -1,14 +1,63 @@
 package com.samsung.learningenglish;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView btn;
+    EditText name;
+    EditText lastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.start_button);
+        name = findViewById(R.id.name);
+        lastName = findViewById(R.id.last_name);
+
+        lastName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(lastName.getText().toString().trim().length() > 0 && name.getText().toString().trim().length() > 0){
+                    btn.setEnabled(true);
+                }
+            }
+        });
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(lastName.getText().toString().trim().length() > 0 && name.getText().toString().trim().length() > 0){
+                    btn.setEnabled(true);
+                }
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LevelsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
